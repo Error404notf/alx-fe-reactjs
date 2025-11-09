e133a71c56db4e5a92ae97fa56d21dd1ac4b43c6
Neon Muthoni <neonmuthoni2@gmail.com>
2025-11-09 23:04:02 +0300

    REFACTOR PROP DRILLING

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
 
 function UserDetails() {
   // Get userData directly from the Context "portal"!
