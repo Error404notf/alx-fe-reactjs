631f0f082aa7500dd535ca3f8bbe91e12bb4c1a7
Neon Muthoni <neonmuthoni2@gmail.com>
2025-11-13 20:31:56 +0300

    fix: add router and routes to App.jsx for ALX checker

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,21 +1,33 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+// import App from './App.jsx';
+// import RecipeDetails from './components/RecipeDetails.jsx';
+
+// function RecipeDetailsWrapper() {
+//   const { id } = useParams();
+//   return <RecipeDetails recipeId={Number(id)} />;
+// }
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import App from './App.jsx';
-import RecipeDetails from './components/RecipeDetails.jsx';
-
-function RecipeDetailsWrapper() {
-  const { id } = useParams();
-  return <RecipeDetails recipeId={Number(id)} />;
-}
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
